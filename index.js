0af6d60eb974d4626ba6e7cc3905e1f88f38e439
Richard Liao <liao098@gmail.com>
2020-02-13 23:32:41 +0000

    add fetchCoordsByIP with error handling

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // index.js
-const { fetchMyIP } = require("./iss");
+const { fetchMyIP, fetchCoordsByIP } = require("./iss");
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -9,3 +9,19 @@ const { fetchMyIP } = require("./iss");
 
 //   console.log("It worked! Returned IP:", ip);
 // });
+
+// const ip = fetchMyIP((error, ip) => {
+//   console.log(`${typeof ip}, ${ip}`);
+//   // return ip;
+// });
+// console.log(ip);
+// const ip = "fuck";
+const ip = "45.55.8.152";
+
+fetchCoordsByIP(ip, (error, coordinates) => {
+  if (error) {
+    console.log(`Error: ${error}`);
+  } else {
+    console.log(`${JSON.stringify(coordinates)}`);
+  }
+});
--- iss.js
+++ iss.js
@@ -26,4 +26,34 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://ipvigilante.com/${ip}`, (error, response, rbody) => {
+    const body = JSON.parse(rbody);
+    // console.log(body.status);
+    // console.log(rbody);
+    if (error) {
+      // console.log(`error: error ${error}`);
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      // console.log(`error: response ${response}`);
+      const msg = `Status Code ${response.statusCode} when fetching IP. 
+                  Response: ${JSON.stringify(body)}`;
+      callback(Error(msg), null);
+      return;
+    }
+    if (body.status === "error") {
+      // console.log(`error: status ${body.status}`);
+      callback(body.status, null);
+    }
+    // console.log(`good: ${body}`);
+    const coordinates = {};
+    coordinates.latitude = body.data.latitude;
+    coordinates.longitude = body.data.longitude;
+    callback(error, coordinates);
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };

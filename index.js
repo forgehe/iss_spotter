a5f8d104277b039852ed705a6965f2458d6f98ad
Richard Liao <liao098@gmail.com>
2020-02-13 22:48:56 +0000

    added iplookup with ipify api

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- */
-const fetchMyIP = function(callback) {
-  // use request to fetch IP address from JSON API
-};
+// index.js
+const { fetchMyIP } = require("./iss");
 
-module.exports = { fetchMyIP };
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log("It worked! Returned IP:", ip);
+// });
